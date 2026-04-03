103df2937013f33d5ebb714745acb2b08eee700a
Pratham Yadav <ypratham0014@gmail.com>
2026-04-03 08:16:17 +0530

    feat: posthog analytics

---
+++ instrumentation-client.ts
@@ -0,0 +1,6 @@
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: "2026-01-30",
+});
